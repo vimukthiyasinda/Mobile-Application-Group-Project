14be6a7e832ef5f54502d7b0a922b8b4c73f9d8b
vimukthiyasinda <vimukthiyasinda007@gmail.com>
2026-02-02 01:23:40 +0530

    Design home page and add app assets.

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -57,13 +57,14 @@ class _FoodPageState extends State<FoodPage> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(10.0),
       child: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
-              padding: EdgeInsets.only(bottom: 10),
+              padding: EdgeInsets.only(bottom: 0),
               child: Text(
                 "What's New",
                 style: TextStyle(
@@ -73,30 +74,36 @@ class _FoodPageState extends State<FoodPage> {
                 ),
               ),
             ),
-            
 
-            Card(
-              shadowColor: Colors.amber,
-              elevation: 4,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: SizedBox(
-                width: 400,
-                height: 200,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(30),
-                  child: Image.asset("assets/ad.png", fit: BoxFit.cover),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Card(
+                  shadowColor: Colors.amber,
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: SizedBox(
+                    width: 400,
+                    height: 200,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: Image.asset("assets/ad.png", fit: BoxFit.cover),
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
-            SizedBox(height: 20),
-           
+
+            SizedBox(height: 5),
+
             Container(
               child: Column(
                 children: [
                   Padding(
-                    padding: EdgeInsets.only(bottom: 10),
+                    padding: EdgeInsets.only(bottom:0),
                     child: Text(
                       "For You",
                       style: TextStyle(
@@ -118,47 +125,144 @@ class _FoodPageState extends State<FoodPage> {
                   shadowColor: Colors.amber,
                   elevation: 4,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadiusGeometry.circular(20),
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                  child: Column(
-                  children: [
-                    Image.asset('assets/cheese.jpg', height: 100, width: 100),
-                    const Text("Cheese Burger", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                    const Text("Delicious Cheese burger"),
-                    const Text("\Rs: 450.00", style: TextStyle(color: Colors.orange)),
-                    const Icon(Icons.fastfood_rounded, size: 35, color: Colors.orange),
-                    SizedBox(height: 5,),
-                    FilledButton(
-                      onPressed: () {},
-                      child: const Text("Click to Order!"),
+                  child: SizedBox(
+                    width: 180,
+                    height: 250,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Image.asset('assets/cheese.jpg', height: 100, width: 100),
+                        const Text("Cheese Burger",
+                          style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),
+                        ),
+                        const Text("Delicious Cheese burger"),
+                        const Text("\Rs: 450.00",
+                          style: TextStyle(color: Colors.orange),
+                        ),
+                        const Icon(
+                          Icons.fastfood_rounded,
+                          size: 35,
+                          color: Colors.orange,
+                        ),
+                        SizedBox(height: 5),
+                        FilledButton(
+                          onPressed: () {},
+                          child: const Text("Click to Order!"),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                  ),
                 ),
 
                 Card(
                   shadowColor: Colors.amber,
                   elevation: 4,
-                  shape: BeveledRectangleBorder(borderRadius: BorderRadiusGeometry.circular(20)),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
 
+                  child: SizedBox(
+                    width: 180, 
+                    height: 250,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Image.asset('assets/cheese.jpg',height: 100,width: 100,),
+                          const Text("Cheese Burger",
+                            style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,),
+                          ),
+                          const Text("Delicious Cheese burger"),
+                          const Text("\Rs: 450.00",
+                            style: TextStyle(color: Colors.orange),
+                          ),
+                          const Icon(
+                            Icons.fastfood_rounded,
+                            size: 35,
+                            color: Colors.orange,
+                          ),
+                          SizedBox(height: 5),
+                          FilledButton(
+                            onPressed: () {},
+                            child: const Text("Click to Order!"),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  shadowColor: Colors.amber,
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(20),
+                  ),
+                  child: SizedBox(
+                    width: 180, // increase width
+                    height: 250,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Image.asset('assets/images.jpeg',height: 100,width: 100,),
+                        const Text("Beef Burger",
+                          style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold,),
+                        ),
+                        const Text("Delicious Beef burger"),
+                        const Text("\Rs: 450.00",
+                          style: TextStyle(color: Colors.orange),
+                        ),
+                        const Icon(
+                          Icons.fastfood_rounded,
+                          size: 35,
+                          color: Colors.orange,
+                        ),
+                        SizedBox(height: 5),
+                        FilledButton(
+                          onPressed: () {},
+                          child: const Text("Click to Order!"),
+                        ),
+                      ],
+                    ),
+                  ),
+                  ),
+                ),
+              ],
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Card(
+                  shadowColor: Colors.amber,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(20),
+                  ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
                   child: Column(
                     children: [
-                      Image.asset('assets/cheese.jpg', height: 100, width: 100),
-                    const Text("Cheese Burger", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                    const Text("Delicious Cheese burger"),
-                    const Text("\Rs: 450.00", style: TextStyle(color: Colors.orange)),
-                    const Icon(Icons.fastfood_rounded, size: 35, color: Colors.orange),
-                    SizedBox(height: 5,),
-                    FilledButton(
-                      onPressed: () {},
-                      child: const Text("Click to Order!"),
-                    ),
+                      Image.asset('assets/chicken.jpeg', height: 100, width: 100),
+                      const Text("Chicken Bucket", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                      const Text("Crispy chicken Bucket"),
+                      const Text("\Rs: 1000.00", style: TextStyle(color: Colors.orange)),
+                      const Icon(Icons.fastfood_rounded, size: 35, color: Colors.orange),
+                      SizedBox(height: 5,),
+                      FilledButton(
+                        onPressed: () {},
+                        child: const Text("Click to Order!"),
+                      ),
                     ],
                   ),
                 ),
-                
+              ),
               ],
-            ),
+            )
           ],
         ),
       ),
