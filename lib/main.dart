9b47c35062526af9a0ce0ffca6d3a80b682d5326
unknown <manujarupasinghe@gmail.com>
2026-02-06 02:01:00 +0530

    create some functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
-import 'signup.dart'; // 👈 මෙන්න මේක අනිවාර්යයෙන්ම තියෙන්න ඕනේ
+/*import 'package:flutter/material.dart';
+import 'signup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SignUpPage(), // 👈 මෙතනින් තමයි Sign Up පිටුවට යන්නේ
+      home: SignUpPage(),
+    );
+  }
+}*/
+
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "McDonald's App",
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: const homepage(),
     );
   }
 }
